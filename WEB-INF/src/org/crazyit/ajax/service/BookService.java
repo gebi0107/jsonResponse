0c3433e13d498da626a64388a0f624bfcf6e4d6e
gebi0107 <gebi0107@gmail.com>
2017-01-07 00:23:35 +0800

    service web

---
+++ WEB-INF/src/org/crazyit/ajax/service/BookService.java
@@ -0,0 +1,33 @@
+package org.crazyit.ajax.service;
+
+import java.util.*;
+
+import org.crazyit.ajax.domain.Book;
+
+public class BookService
+{
+	// 模拟内存中数据库
+	static Map<Integer , List<Book>> bookDb =
+		new LinkedHashMap<>();
+	static
+	{
+		// 初始化bookDb对象
+		List<Book> list1 = new ArrayList<>();
+		List<Book> list2 = new ArrayList<>();
+		List<Book> list3 = new ArrayList<>();
+		list1.add(new Book(1 , "疯狂Java讲义" , "李刚" , 109));
+		list1.add(new Book(2 , "轻量级Java EE企业应用实战" , "李刚" , 99));
+		list1.add(new Book(3 , "疯狂Android讲义" , "李刚" , 89));
+		list2.add(new Book(4 , "西游记" , "吴承恩" , 23));
+		list2.add(new Book(5 , "水浒" , "施耐庵" , 20));
+		list3.add(new Book(6 , "乌合之众" , "古斯塔夫.勒庞" , 16));
+		list3.add(new Book(7 , "不合时宜的考察" , "尼采" , 18));
+		bookDb.put(1 , list1);
+		bookDb.put(2 , list2);
+		bookDb.put(3 , list3);
+	}
+	public List<Book> getBookByCategory(int categoryId)
+	{
+		return bookDb.get(categoryId);
+	}
+}
---
+++ WEB-INF/src/org/crazyit/ajax/web/ChooseBookServlet.java
@@ -0,0 +1,29 @@
+package org.crazyit.ajax.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax. servlet.annotation.*;
+
+import java.io.*;
+import java.util.*;
+import org.json.*;
+
+import org.crazyit.ajax.domain.*;
+import org.crazyit.ajax.service.*;
+
+@WebServlet(urlPatterns = {"/chooseBook"})
+
+public class ChooseBookServlet extends HttpServlet {
+	
+		public void service(HttpServletRequest request, HttpServletResponse response) 
+		throws IOException, ServletException
+		{
+			
+				String idStr = (String)request.getParameter("id");
+				int id = idStr == null ? 1 : Integer.parseInt(idStr);
+				List<Book> books = new BookService().getBookByCategory(id);
+				response.setContentType("text/html;charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				out.println(new JSONArray(books));
+			}
+	}
